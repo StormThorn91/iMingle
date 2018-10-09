7102654b24912a26146be50bbb257d61dedbce71
Justine Paul Sanchez Vitan <justine@Justines-iMac.local>
2018-10-09 11:32:39 +0800

    Add LogInViewController

---
+++ iMingle/LogInViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LogInViewController.swift
+//  iMingle
+//
+//  Created by Justine Paul Sanchez Vitan on 09/10/2018.
+//  Copyright © 2018 CGJ. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var textFieldUsername: EHTextField!
+    @IBOutlet weak var textFieldPassword: EHTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func buttonLogInClicked(_ sender: Any) {
+    
+    }
+    
+    @IBAction func buttonSignUpClicked(_ sender: Any) {
+    
+    }
+}
